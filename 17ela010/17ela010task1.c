2498c8cb0362cd5535b4e4b0834d3a13ebd03160
TarriestPython <TarriestPython@users.noreply.github.com>
2017-11-17 17:54:01 +0000

    Added a basic command window user interface to allow the user to choose: The dimesnsions of the output image The name of the output image The number of harmonics used to generate the output image

--- 17ela010/17ela010task1.c
+++ 17ela010/17ela010task1.c
@@ -5,32 +5,62 @@
 #include <math.h>
 
 void main()
-{
-	int outWidth = 1920;
-	int outHeight = 1080;
-	int nHarmonics = 10;
+{	
 	//Create the header for the ppm file header,
 	char pmmHeader[] = "P3\n%d %d\n255\n";
-	//declare an array of pixels the size of the output picture defulting all values to 0,
-	int* pixels = calloc(outWidth * outHeight, sizeof(int));
-	//declare the poiter for the file
-	FILE *pfile = NULL;
+	// Create variables for user to declare;
+	int outWidth;
+	int outHeight;
+	int nHarmonics;
+	char fileName[32];
 
-	DrawWaves(nHarmonics, pixels, outWidth, outHeight);
-	// Draw x axis in white
-	for (int x = 0; x < outWidth; x++)
+	// Get the user input for the number of harmonics to use
+	printf("Input -1 to exit the program.\nInput the number of harmonics you would like to use to generate the square wave: ");
+	scanf("%d", &nHarmonics);
+
+	while (nHarmonics > 0)
 	{
-		pixels[(outHeight / 2) * outWidth + x] = rgbInt(255, 255, 255);
+		// Get dimensions of the output image from the user
+		printf("Input the width of the output image: ");
+		scanf("%d", &outWidth);
+		printf("Input the height of the output image: ");
+		scanf("%d", &outHeight);
+
+		// Get the user to input the file to write to
+		printf("Input the name of the file you would like to draw to (DO NOT USE SPACES): ");
+		scanf("%s", &fileName);
+
+		printf("Allocating memory for pixel array...\n");
+		//declare an array of pixels the size of the output picture defulting all values to 0,
+		int* pixels = calloc(outWidth * outHeight, sizeof(int));
+
+		printf("Calculating and drawing waves to the array...\n");
+		DrawWaves(nHarmonics, pixels, outWidth, outHeight);
+		// Draw x axis in white
+		printf("Drawing x axis on image...\n");
+		for (int x = 0; x < outWidth; x++)
+		{
+			pixels[(outHeight / 2) * outWidth + x] = rgbInt(255, 255, 255);
+		}
+
+		// Open the file
+		printf("Opening and writing to file...\n");
+		FILE *pfile = fopen(fileName, "w");
+		// Write to the file
+		fprintf(pfile, pmmHeader, outWidth, outHeight);
+		DrawPixelArray(pfile, pixels, outWidth, outHeight);
+		
+		printf("Closing file and freeing memory...\n");
+		// Close the file and set the pointer to NULL so it cannot be accessed later
+		fclose(pfile);
+		pfile = NULL;
+		// Free the memory used to store the pixels and set the pointer to it NULL so it cannot be accessed later
+		free(pixels);
+		pixels = NULL;
+		printf("File Generated!\n\n");
+
+		// Get the user input for the number of harmonics to use
+		printf("Input -1 to exit the program.\nInput the number of harmonics you would like to use to generate the square wave: ");
+		scanf("%d", &nHarmonics);
 	}
-	// Open the file
-	pfile = fopen("myfile.ppm", "w");
-	// Write to the file
-	fprintf(pfile, pmmHeader, outWidth, outHeight);
-	DrawPixelArray(pfile, pixels, outWidth, outHeight);
-	// Close the file and set the pointer to NULL so it cannot be accessed later
-	fclose(pfile);
-	pfile = NULL;
-	// Free the memory used to store the pixels and set the pointer to it NULL so it cannot be accessed later
-	free(pixels);
-	pixels = NULL;
 }
