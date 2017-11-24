7b879a4bf19b24da25ab6fb1db56ee484834dc63
eljp <eljp@W7-STEM105-27>
2017-11-24 17:29:31 +0000

    Added user input validation to prevent the program from crashing when the user inputs an invalid type. Still an issue if the user inputs whitespace (space characters) that needs to be fixed. Also updated gitignore to exclude ppm files from the commit.

--- 17ela010/17ela010task1.c
+++ 17ela010/17ela010task1.c
@@ -14,7 +14,7 @@ Date:	21-11-17
 #include <math.h>
 
 void main()
-{	
+{
 	//Create the header for the ppm file header,
 	char pmmHeader[] = "P3\n%d %d\n255\n";
 	// Create variables for user to declare;
@@ -24,22 +24,58 @@ void main()
 	char fileName[32];
 	FILE* pfile = NULL;
 	int* pixels = NULL;
-
+	char inputErrorBuffer[64] = { 0 };
+	// Boolean to be used for input stages to ensure the user inputs valid data
+	enum bool valid;
+	
 	// Get the user input for the number of harmonics to use
-	printf("Input 0 or negative to exit the program.\nInput the number of harmonics you would like to use to generate the square wave: ");
-	scanf("%d", &nHarmonics);
+	do
+	{
+		// Set the initial state of the valid bool to be true
+		valid = true;
+		// Prompt the user for input
+		printf("Input 0 or negative to exit the program.\nInput the number of harmonics you would like to use to generate the square wave: ");
+		// Get the input and check if the return code is 0 or negative (invalid input)
+		if (scanf("%d", &nHarmonics) <= 0)
+		{
+			// If it was read the input into a string and return an error to the user
+			scanf("%s", &inputErrorBuffer);
+			printf("Your input of:\n\"%s\"\nis invalid. Please re-enter\n\n", inputErrorBuffer);
+			// Set the valid bool to false so that the user is prompted for input again
+			valid = false;
+		}
+	} while (valid == false);
 
 	while (nHarmonics > 0)
 	{
-		// Get dimensions of the output image from the user
-		printf("Input the width of the output image: ");
-		scanf("%d", &outWidth);
-		printf("Input the height of the output image: ");
-		scanf("%d", &outHeight);
-		int done;
+		// Get dimensiosn of the output image from the user
+		do
+		{	
+			valid = true;
+			printf("Input the width of the output image: ");
+			if (scanf("%d", &outWidth) <= 0)
+			{
+				scanf("%s", &inputErrorBuffer);
+				printf("Your input of:\n\"%s\"\nis invalid. Please re-enter\n\n", inputErrorBuffer);
+				valid = false;
+			}
+		} while (valid == false);
+
 		do
 		{
-			done = 1;
+			valid = true;
+			printf("Input the height of the output image: ");
+			if (scanf("%d", &outHeight) <= 0)
+			{
+				scanf("%s", &inputErrorBuffer);
+				printf("Your input of:\n\"%s\"\nis invalid. Please re-enter\n\n", inputErrorBuffer);
+				valid = false;
+			}
+		} while(valid == false);
+
+		do
+		{
+			valid = true;
 			// Get the user to input the file to write to
 			printf("Input the name of the file you would like to draw to (DO NOT USE SPACES): ");
 			scanf("%s", &fileName);
@@ -48,16 +84,16 @@ void main()
 			// Open the file
 			printf("Opening the file...\n");
 			pfile = fopen(fileName, "w");
-			
+
 			//Check opening the file worked
 			if (pfile == NULL)
 			{
 				// if it didnt work return an error to the user asking them to input a different file name
 				printf("Something went wrong opening the file\nTry A different file name using NO WHITESPACE\n");
-				done = 0;
+				valid = false;
 			}
-		// loop until opening the file was successful
-		} while(done == 0);
+			// loop until opening the file was successful
+		} while (valid == false);
 
 		printf("Allocating memory for pixel array...\n");
 		//declare an array of pixels the size of the output picture defulting all values to 0,
@@ -76,7 +112,7 @@ void main()
 		printf("Writing to the file...\n");
 		fprintf(pfile, pmmHeader, outWidth, outHeight);
 		DrawPixelArray(pfile, pixels, outWidth, outHeight);
-		
+
 		printf("Closing file and freeing memory...\n");
 		// Close the file and set the pointer to NULL so it cannot be accessed later
 		fclose(pfile);
@@ -87,7 +123,16 @@ void main()
 		printf("File Generated!\n\n");
 
 		// Get the user input for the number of harmonics to use
-		printf("Input 0 or negative to exit the program.\nInput the number of harmonics you would like to use to generate the square wave: ");
-		scanf("%d", &nHarmonics);
+		do
+		{
+			valid = true;
+			printf("Input 0 or negative to exit the program.\nInput the number of harmonics you would like to use to generate the square wave: ");
+			if (scanf("%d", &nHarmonics) <= 0)
+			{
+				scanf("%s", &inputErrorBuffer);
+				printf("Your input of:\n\"%s\"\nis invalid. Please re-enter\n\n", inputErrorBuffer);
+				valid = false;
+			}
+		} while (valid == false);
 	}
 }
